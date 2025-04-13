8ed4a756abd3c4178653ad1ea7f1dea59d47117b
Dev-Mriganka <mrigankamondal10@gmail.com>
2025-04-13 16:08:54 +0530

    Update SocialMediaContent type name for consistency in ContentDetails

--- src/main/java/com/smartcms/smartcommon/model/content/ContentDetails.java
+++ src/main/java/com/smartcms/smartcommon/model/content/ContentDetails.java
@@ -6,7 +6,7 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo;
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "type")
 @JsonSubTypes({
         @JsonSubTypes.Type(value = BlogPostContent.class, name = "BLOG_POST"),
-        @JsonSubTypes.Type(value = SocialMediaContent.class, name = "SOCIAL_MEDIA_POST"),
+        @JsonSubTypes.Type(value = SocialMediaContent.class, name = "SOCIAL_MEDIA"),
         @JsonSubTypes.Type(value = NewsletterContent.class, name = "NEWSLETTER"),
         @JsonSubTypes.Type(value = LandingPageContent.class, name = "LANDING_PAGE"),
         @JsonSubTypes.Type(value = PromotionalEmailContent.class, name = "PROMOTIONAL_EMAIL"),
